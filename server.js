6c833e1005ab26ce63e5826fc1937dc26f56837d
Cristi Mihai <cristi.mihai@gmail.com>
2012-03-13 15:40:02 +0000

    ssl

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ var public = "web/";
 
 // IMPORTS ______________________________________________________
 
-var http = require("http");
-var fs = require("fs");
-var express = require("express");
-var site = express.createServer();
-var url = require("url");
+var express = require("express"),
+    crypto = require("crypto"),
+    fs = require("fs");
+
+var http = express.createServer();
+var https = express.createServer({
+    key: fs.readFileSync("privatekey.pem"),
+    cert: fs.readFileSync("certificate.pem")
+});
 
 //_______________________________________________________________
 // IO
 
-var io = require('socket.io').listen(site);
+var io = require('socket.io').listen(http);
 
 //io.set('transports', [
 //    'websocket',
@@ -31,70 +35,75 @@ var server = require("./server/server");
 var s = new Server({io: io});
 
 
-//_______________________________________________________________
-// Static resources
+var register = function (app) {
 
-site.use("/", express.static(public));
+
+    app.use("/", express.static(public));
+
+
+    app.post('/fb/', function(req, res) {
+        var signed_request = req.param('signed_request');
+        if (!signed_request) {
+            res.send("Request not signed.");
+            res.end();
+        }
+        var parts = signed_request.split('.');
+        var sig = base64UrlToBase64(parts[0]);
+        var payload = parts[1];
+        var data = JSON.parse(base64UrlToString(payload));
+        if (!data.user_id) {
+            // send over to authorize url
+        }
+        else {
+            // lets verify
+            if (data.algorithm.toUpperCase() !== 'HMAC-SHA256') {
+                res.send('Unknown algorithm. Expected HMAC-SHA256');
+                return;
+            }
+            var secret = 'f96d4be66d931678b7c5f12ee02e8db4';
+            var hmac = require('crypto').createHmac('sha256', secret);
+            hmac.update(payload);
+            var expected_sig = hmac.digest('base64');
+            if (sig != expected_sig){
+                console.log('expected [' + expected_sig + '] got [' + sig + ']');
+                res.send('Hello, this is my app! you are CHEATING! .. expected [' + expected_sig + '] got [' + sig + ']');
+            }
+            else {
+                res.send('Hello, this is my app! you passed verification and are ' + data.user_id);
+            }
+        }
+    });
+
+};
 
 //_______________________________________________________________
 // Facebook
 
 var base64ToString = function(str) {
-	return (new Buffer(str || "", "base64")).toString("ascii");
+    return (new Buffer(str || "", "base64")).toString("ascii");
 };
 
 var base64UrlToString = function(str) {
-	return base64ToString( base64UrlToBase64(str) );
+    return base64ToString( base64UrlToBase64(str) );
 };
 
 var base64UrlToBase64 = function(str) {
-	var paddingNeeded = (4- (str.length%4));
-	for (var i = 0; i < paddingNeeded; i++) {
-		str = str + '=';
-	}
-	return str.replace(/\-/g, '+').replace(/_/g, '/')
+    var paddingNeeded = (4- (str.length%4));
+    for (var i = 0; i < paddingNeeded; i++) {
+        str = str + '=';
+    }
+    return str.replace(/\-/g, '+').replace(/_/g, '/')
 };
-
-site.post('/fb/', function(req, res) {
-    var signed_request = req.param('signed_request');
-    if (!signed_request) {
-        res.send("Request not signed.");
-        res.end();
-    }
-    var parts = signed_request.split('.');
-    var sig = base64UrlToBase64(parts[0]);
-    var payload = parts[1];
-    var data = JSON.parse(base64UrlToString(payload));
-    if (!data.user_id) {
-        // send over to authorize url
-    }
-    else {
-        // lets verify
-        if (data.algorithm.toUpperCase() !== 'HMAC-SHA256') {
-            res.send('Unknown algorithm. Expected HMAC-SHA256');
-            return;
-        }
-        var secret = 'f96d4be66d931678b7c5f12ee02e8db4';
-        var hmac = require('crypto').createHmac('sha256', secret);
-        hmac.update(payload);
-        var expected_sig = hmac.digest('base64');
-        if (sig != expected_sig){
-            console.log('expected [' + expected_sig + '] got [' + sig + ']');
-            res.send('Hello, this is my app! you are CHEATING! .. expected [' + expected_sig + '] got [' + sig + ']');
-        }
-        else {
-            res.send('Hello, this is my app! you passed verification and are ' + data.user_id);
-        }
-    }
-});
-
 
 //_______________________________________________________________
 
 
-var port = process.env.PORT || 8000;
+register(http);
+http.listen(8000);
 
-site.listen(port);
+register(https);
+https.listen(8443);
 
-console.log("Server listening on :" + port);
+console.log("Server started.");
+
 
