1574e7b1c10886b848b211ba7333bc090eb9c943
Cristi Mihai <cristi.mihai@gmail.com>
2012-03-15 13:38:26 +0000

    monitor sounds

--- web/monitor/js/main.js
+++ web/monitor/js/main.js
@@ -22,18 +22,27 @@ require([
 
         var socket = io.connect('/monitoring');
 
+        var oldUsers = 0;
+
         socket.on('stat', function(stat) {
 
             chart("cpu", stat.cpu, stat.cpu+'%', {chartRangeMin: 0, chartRangeMax: 100, fillColor: '#0f0'});
             chart("users", stat.users, stat.users, {chartRangeMin: 0, fillColor: '#339'});
             chart("mapfill", stat.mapfill, stat.mapfill+'%', {chartRangeMin: 0, fillColor: '#933'});
 
+            if (oldUsers < stat.users) {
+                play("player-up");
+            }
+            oldUsers = stat.users;
+
             if (stat.cpu < 20)
                 $("#curent-cpu").css({'color': '#0f0'});
             else if (stat.cpu < 60)
                 $("#curent-cpu").css({'color': '#fa0', 'font-size': '110%'});
-            else
+            else {
                 $("#curent-cpu").css({'color': '#f00', 'font-size': '120%'});
+                alertSound();
+            }
         });
 
     });
@@ -51,6 +60,15 @@ require([
         $('#chart-'+n).sparkline(vs, _.extend(opt, {width: '100px', height: '30px'}));
 
         $("#curent-"+n).text(tv);
+    }
+
+    var alertSound = _.throttle(function() {
+        play("alert");
+    }, 10000);
+
+    function play(snd) {
+        var a = new Audio("res/" + snd + ".wav");
+        a.play();
     }
 
 });
