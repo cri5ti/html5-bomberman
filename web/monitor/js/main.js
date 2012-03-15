53234426ad003ddb353f609f957ea6a2101d37ca
Cristi Mihai <cristi.mihai@gmail.com>
2012-03-15 13:01:19 +0000

    monitor styling

--- web/monitor/js/main.js
+++ web/monitor/js/main.js
@@ -24,16 +24,22 @@ require([
 
         socket.on('stat', function(stat) {
 
-            chart("cpu", stat.cpu, {val: stat.cpu+'%', chartRangeMin: 0, chartRangeMax: 100, fillColor: '#dfd'});
-            chart("users", stat.users, {chartRangeMin: 0, fillColor: '#ddf'});
-            chart("mapfill", stat.mapfill, {val: stat.mapfill+'%', chartRangeMin: 0, fillColor: '#ffd'});
+            chart("cpu", stat.cpu, stat.cpu+'%', {chartRangeMin: 0, chartRangeMax: 100, fillColor: '#0f0'});
+            chart("users", stat.users, stat.users, {chartRangeMin: 0, fillColor: '#339'});
+            chart("mapfill", stat.mapfill, stat.mapfill+'%', {chartRangeMin: 0, fillColor: '#933'});
 
+            if (stat.cpu < 20)
+                $("#curent-cpu").css({'color': '#0f0'});
+            else if (stat.cpu < 60)
+                $("#curent-cpu").css({'color': '#fa0', 'font-size': '110%'});
+            else
+                $("#curent-cpu").css({'color': '#f00', 'font-size': '120%'});
         });
 
     });
 
     var vals = {};
-    var chart = function(n, v, opt) {
+    var chart = function(n, v, tv, opt) {
         if (!vals[n]) vals[n] = [];
         var vs = vals[n];
 
@@ -42,11 +48,9 @@ require([
 
         vs.push(v);
 
-        $('#chart-'+n).sparkline(vs, _.extend(opt, {
-            height: '50px'
-        }));
+        $('#chart-'+n).sparkline(vs, _.extend(opt, {width: '100px', height: '30px'}));
 
-        $("#curent-"+n).text(opt.val || v);
+        $("#curent-"+n).text(tv);
     }
 
 });
